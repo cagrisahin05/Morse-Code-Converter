52274c8eac040fabe8fa1b588eb7cf58928191ab
cagrisahin05 <cagrisahin21s@gmail.com>
2025-02-27 17:10:14 +0300

    One letter

---
+++ MorseCodeConverter/Assets/Scripts/Converter.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Converter : MonoBehaviour
+{
+
+    Dictionary<string, string> latinToMorse;
+    public TMPro.TMP_InputField inputField;
+    public TMPro.TMP_Text outputText;
+
+    void Start()
+    {
+        CeviriKarsiligi();
+        inputField.onEndEdit.AddListener(delegate { if (Input.GetKeyDown(KeyCode.Return)) Cevirmeİslemi(); });
+    }
+
+
+    void Cevirmeİslemi()
+    {
+        string latinCharacter = inputField.text.ToLower();
+        int length = latinCharacter.Length;
+        for (int i = 0; i < length; i++)
+        {
+
+            if (latinToMorse.TryGetValue(latinCharacter, out string morseTranslation))
+            {
+                outputText.text = morseTranslation;
+
+            }
+            else
+            {
+                outputText.text = "Gecersiz";
+
+            }
+        }
+
+    }
+
+    void CeviriKarsiligi()
+    {
+        latinToMorse = new Dictionary<string, string>();
+        latinToMorse.Add("a", ".-");
+        latinToMorse.Add("b", "-...");
+        latinToMorse.Add("c", "-.-.");
+        latinToMorse.Add("d", "-..");
+        latinToMorse.Add("e", ".");
+        latinToMorse.Add("f", "..-.");
+        latinToMorse.Add("g", "--.");
+        latinToMorse.Add("h", "....");
+        latinToMorse.Add("i", "..");
+        latinToMorse.Add("j", ".---");
+        latinToMorse.Add("k", "-.-");
+        latinToMorse.Add("l", ".-..");
+        latinToMorse.Add("m", "--");
+        latinToMorse.Add("n", "-.");
+        latinToMorse.Add("o", "---");
+        latinToMorse.Add("p", ".--.");
+        latinToMorse.Add("q", "--.-");
+        latinToMorse.Add("r", ".-.");
+        latinToMorse.Add("s", "...");
+        latinToMorse.Add("t", "-");
+        latinToMorse.Add("u", "..-");
+        latinToMorse.Add("v", "...-");
+        latinToMorse.Add("w", ".--");
+        latinToMorse.Add("x", "-..-");
+        latinToMorse.Add("y", "-.--");
+        latinToMorse.Add("z", "--..");
+        latinToMorse.Add("1", ".----");
+        latinToMorse.Add("2", "..---");
+        latinToMorse.Add("3", "...--");
+        latinToMorse.Add("4", "....-");
+        latinToMorse.Add("5", ".....");
+        latinToMorse.Add("6", "-....");
+        latinToMorse.Add("7", "--...");
+        latinToMorse.Add("8", "---..");
+        latinToMorse.Add("9", "----.");
+        latinToMorse.Add("0", "-----");
+        latinToMorse.Add(" ", " ");
+    }
+
+
+}
